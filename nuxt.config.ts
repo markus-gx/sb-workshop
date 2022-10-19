2f0b3d468d5797a6888f0950e0f763bd42f48cbd
Markus Geilehner <markus@geilehner.io>
2022-10-19 09:57:27 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            meta: [],
+            link: [],
+        },
+    },
     modules: [
         ["@storyblok/nuxt", { accessToken: process.env.STORYBLOK_API_KEY }],
         '@nuxtjs/tailwindcss'
-    ]
+    ],
+    components: [
+        'components/atoms',
+        'components/molecules',
+        'components/organisms',
+    ],
 })
